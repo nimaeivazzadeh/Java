6da17c25192d53b7e83495bfb4eab807afce2aa5
nimaeivazzadeh <nima.eivazzadehkaljahi@yahoo.com>
2021-02-26 16:18:25 +0100

    first commit

---
+++ src/practice/main.java
@@ -0,0 +1,12 @@
+package practice;
+
+import practice1.Practice;
+
+public class main {
+    public static void main(String[] args) {
+      Practice practice = new Practice(": eivazzadeh");
+      Practice practice1 = new Practice(": nima");
+      Practice practice2 = new Practice(": kaljahi");
+
+    }
+}
---
+++ src/practice1/Practice.java
@@ -0,0 +1,19 @@
+package practice1;
+
+public class Practice {
+ public Practice(String name)
+ {
+     System.out.println("I am a constructor" + name);
+ }
+ public void a()
+ {
+   System.out.println("a");
+ }
+ public void b()
+ {
+   System.out.println("b");
+ }
+}
+
+
+
